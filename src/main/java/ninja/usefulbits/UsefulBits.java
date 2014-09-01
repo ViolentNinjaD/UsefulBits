19320830633d4aefa18ee85866eb15088edfe257
ViolentNinjaD <fiddlybilly@gmail.com>
2014-09-01 18:21:37 +0100

    Working Stuffs :)

---
+++ src/main/java/ninja/usefulbits/UsefulBits.java
@@ -0,0 +1,12 @@
+package ninja.usefulbits;
+//Copyright ViolentNInjaD 2014
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = UsefulBits.MOD_ID, name = UsefulBits.MOD_NAME, version = UsefulBits.MOD_VERSION)
+public class UsefulBits
+{
+    public static final String MOD_ID = "UsefulBits";
+    public static final String MOD_NAME = "Useful Bits";
+    public static final String MOD_VERSION = "1.7.10-01";
+}
