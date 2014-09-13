77cdf3193a002d2f505060a6ee1395c1f741a2d5
ViolentNinjaD <fiddlybilly@gmail.com>
2014-09-13 09:54:13 +0100

    Fixed a build error. This will be the release mark for 1.7.10-03

--- src/main/java/ninja/usefulbits/init/UBCraftingRecipes.java
+++ src/main/java/ninja/usefulbits/init/UBCraftingRecipes.java
@@ -1,9 +1,9 @@
 package ninja.usefulbits.init;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import violentninjad.violentapi.api.CraftingHelper;
 
 /*@author ViolentNinjaD
 
@@ -14,7 +14,7 @@ public class UBCraftingRecipes
 {
     public static void init()
     {
-        CraftingHelper.standard(new ItemStack(UBBlocks.darkenedBlock, 1), "ddd", "ddd", "ddd", 'd', UBItems.darkenedIngot);
-        CraftingHelper.standard(new ItemStack(UBItems.darkenedIngot, 1), "id ", "   ", "   ", 'd', Items.iron_ingot, 'i', new ItemStack(Items.dye, 1, 0));
+        GameRegistry.addRecipe(new ItemStack(UBBlocks.darkenedBlock, 1), "ddd", "ddd", "ddd", 'd', UBItems.darkenedIngot);
+        GameRegistry.addRecipe(new ItemStack(UBItems.darkenedIngot, 1), "id ", "   ", "   ", 'd', Items.iron_ingot, 'i', new ItemStack(Items.dye, 1, 0));
     }
 }
